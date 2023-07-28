589ecbf193144ad9ee0d6bdd63ec79376d7ec680
NicoDelgadoCentennial <ndelgad4@my.centennialcollege.ca>
2023-07-28 18:55:03 -0400

    Login changes added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,8 @@
 module.exports = (app)=>{
   const user = require('../controllers/user.controller');
 
-  app.post('/users', user.create);
-  app.post('/users/userName/', user.findOne);
+  app.post('/register', user.create);
+  app.post('/login/', user.findOne);
   app.get('/users', user.findAll);
   //app.get('/users/:id', user.findOne);
   app.put('/users/:id', user.update);
